5df896c2eec9cd03cbdd4fab620d923a43c30c2e
gurucharan.chouhan <gurucharan.chouhan@skuad.io>
2025-08-03 18:49:34 +0530

    DOC-002: Add Doctor Rating feature

--- src/doctor/doctor.controller.ts
+++ src/doctor/doctor.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post} from '@nestjs/common';
 import { DoctorService } from './doctor.service';
 
 @Controller('doctors') // <-- This maps to /doctors
 export class DoctorController {
   constructor(private readonly doctorService: DoctorService) {}
 
-  @Get()
+  @Get('allDoctors')
   async getAllDoctors() {
     return this.doctorService.getDoctors();
+  }
+
+  @Post()
+  async createDoctor(
+    @Body() body: { name: string; specialization: string },
+  ) {
+    return this.doctorService.createDoctor(body);
+  }
+
+  @Post('addRating')
+  async rateDoctor(@Body() body: { doctId: string; ratingCount: number; ratingDesc: string }) {
+    return this.doctorService.addDoctorRating(body);
   }
 }
